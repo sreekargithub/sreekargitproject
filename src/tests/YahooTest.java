17f54b798fffbd229a053dfc1c983c5818c8e3ed
npavan3 <npavan3@gmail.com>
2014-09-24 12:37:06 +0530

    Update YahooTest.java

--- src/tests/YahooTest.java
+++ src/tests/YahooTest.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+//Edited by Pavan
 public class YahooTest {
 	WebDriver driver;
 	@Before
