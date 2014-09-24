7d16123598b4fe442ad81f89047c9718012846e5
anuhyamalla <anuhyamalla@gmail.com>
2014-09-24 12:23:35 +0530

    anuhya naidu

--- src/tests/AllTests.java
+++ src/tests/AllTests.java
@@ -4,6 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+
+//Anuhya
 @RunWith(Suite.class)
 @SuiteClasses({FacebookTest.class, GmailTest.class, YahooTest.class})
 public class AllTests {
