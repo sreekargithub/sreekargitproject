88e26583c63c56e9b479dc5b82c2e18230db6eb3
ameernaveed7 <ameernaveed77@gmail.com>
2014-09-24 12:07:41 +0530

    ameernaveed

--- src/tests/AllTests.java
+++ src/tests/AllTests.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+// this is the git hub testing modification
 @RunWith(Suite.class)
 @SuiteClasses({FacebookTest.class, GmailTest.class, YahooTest.class})
 public class AllTests {
